2fab91f6913d2ec67bebf051490a4680da67d143
YuraYarmola <uarmola1@gmail.com>
2024-05-12 12:33:31 +0300

    change thms

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,7 +1,10 @@
+import json
+
 from rest_framework import status, generics, response
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import UpdateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
 from .serializers import *
 
@@ -12,7 +15,6 @@ class MissingPersonListCreateAPIView(generics.ListCreateAPIView, ):
     serializer_class = MissingPersonSerializer
     filter_backends = [SearchFilter]
     search_fields = ['first_name', 'last_name']
-
 
 class MissingPersonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AllowAny]
